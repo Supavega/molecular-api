9b501d0f2ca27edaea08a0f6e10db9e451971445
ThomasGrare <thomasgrare64@gmail.com>
2024-04-22 16:49:18 +0200

    authentification

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import * as userController from "./controller/userController.js"
+
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,10 @@ app.use(cors({
   credentials: true,
   exposedHeaders: ['Authorization'],
 }));
+
+//POST Routes
+app.post("/register", userController.register);
+app.post("/login", userController.login);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
---
+++ schema/user.js
@@ -0,0 +1,59 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import Joi, { required } from "joi";
+
+const userSchema = new Schema({
+    mail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    role: {
+      type: Number,
+      required: true
+    }
+}, {timestamps: true});
+
+
+// Hash password
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Compare the given password with the hashed password in the database
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.joiValidate = (user) => {
+  const schema = Joi.object({
+    mail: Joi.string().email().required(),
+    username: Joi.string().min(8).max(30).required(),
+    password: Joi.string().min(8).max(30).regex(/[a-zA-Z0-9]{3,30}/).required(),
+    role: Joi.number().max(0).min(2).required()
+  });
+  return schema.validate(user);
+};
+
+export default mongoose.model('User', userSchema);
