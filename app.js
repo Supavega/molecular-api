a2bd9a2122f57f23d208bd90e4697487a6489245
ThomasGrare <thomasgrare64@gmail.com>
2024-05-05 11:29:48 +0200

    wip workspace api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import * as userController from "./controller/userController.js";
 import * as workspaceController from "./controller/workspaceController.js";
+import jwtVerify from "./middleware/jwtVerify.js";
 
 
 dotenv.config();
@@ -21,23 +22,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //GET Routes
-app.get("/workspace", workspaceController.getWorkspace);
-app.get("/workspace/:id", workspaceController.getSpecificWorkspace);
+app.get("/workspace", jwtVerify, workspaceController.getWorkspace);
 
 //POST Routes
 app.post("/register", userController.register);
 app.post("/login", userController.login);
-app.post("/workspace", workspaceController.createWorkspace);
+app.post("/workspace", jwtVerify, workspaceController.createWorkspace);
 
 
 //DELETE Routes
-app.delete("/workspace", workspaceController.deleteWorkspace);
-
+app.delete("/workspace", jwtVerify, workspaceController.deleteWorkspace);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((err) => console.log("Connexion à MongoDB échouée !", err));
-
 
 app.listen(8080, () => {
   console.log("Serveur démarré sur le port 8080");
